d588453c609441941e292aa678b20ebcccb7bdf6
Unknown <tirtajiwanggataufiq@gmail.com>
2017-12-07 05:55:22 +0700

    add mode

--- md5sum.c
+++ md5sum.c
@@ -1,8 +1,10 @@
 #include "CustomMD5Header.h"
 #include "printf.c"
 
+void md5string(md5context_t * ctx, char * input);
+
 int main(int argc, char **argv){
-    if(argc < 2){
+    if(argc < 2 || strcmp(argv[1],"-s")!=0 || strcmp(argv[1],"-f")==0){
         printf(1,"Usage:\n");
         printf(1,"-s: digest string\n");
         printf(1,"-f: digest file\n");
@@ -13,7 +15,18 @@ int main(int argc, char **argv){
 
     }
     else if(strcmp(argv[1],"-f")==0){
-        
+
     }
     return 0;
+}
+
+void md5string(md5context_t * ctx, char * input){
+    uint32 len = strlen(input);
+    byte result[16];
+    Init_Context(ctx);
+    Update_Context(ctx, input, len);
+
+    printf(1,"%s",result);
+    putc(1,"\n");
+    return;
 }
